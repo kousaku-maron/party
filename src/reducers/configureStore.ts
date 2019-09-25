29ccf2a466fd63d8e5b93aae89b668c1e45474b9
kosaku-kurino <kurino.kosaku@kmw.jp>
2019-09-25 18:50:36 +0900

    feat: create actions and reducers

---
+++ src/reducers/configureStore.ts
@@ -0,0 +1,23 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import { authReducer, Auth } from './auth'
+
+const middlewares = []
+
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { logger } = require('redux-logger')
+  middlewares.push(logger)
+}
+
+type AppState = {
+  auth: Auth
+}
+
+const reducers = combineReducers<AppState>({ auth: authReducer })
+
+const configureStore = (initialState: AppState) => {
+  const store = createStore(reducers, initialState, applyMiddleware(...middlewares))
+  return store
+}
+
+export default configureStore
