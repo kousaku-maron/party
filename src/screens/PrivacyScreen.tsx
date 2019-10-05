120cb67998a20b8c15b4886d396ce45ab01933ac
kosaku kurino <kousaku.maron@gmail.com>
2019-10-05 19:43:40 +0900

    feat: create terms and privacy screen

---
+++ src/screens/PrivacyScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { WebView } from 'react-native-webview'
+
+const PrivacyScreen = () => {
+  return <WebView source={{ uri: 'https://insta-693eb.web.app/webview-privacy' }} />
+}
+
+PrivacyScreen.navigationOptions = () => ({
+  headerTitle: 'プライバシー'
+})
+
+export default PrivacyScreen
