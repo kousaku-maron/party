bc00d23f87ee7f4b79ef3dea921a92755ef01791
tabikaeru <moriokayui@eisti.eu>
2019-10-06 04:31:36 +0900

    feat: add fetch party's humbnailURL on the home screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,4 +3,3 @@ export { default as WelcomeScreen } from './WelcomeScreen'
 export { default as HomeScreen } from './HomeScreen'
 export { default as PostScreen } from './PostScreen'
 export { default as UserScreen } from './UserScreen'
-//coment
