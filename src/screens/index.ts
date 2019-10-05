feefab24af8d2cfe450119ed6773860a8910f096
tabikaeru <moriokayui@eisti.eu>
2019-10-05 20:51:20 +0900

    test commit

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,3 +3,4 @@ export { default as WelcomeScreen } from './WelcomeScreen'
 export { default as HomeScreen } from './HomeScreen'
 export { default as PostScreen } from './PostScreen'
 export { default as UserScreen } from './UserScreen'
+//coment
