413297d9d0965c81e1eb5870041124e1a2529159
kosaku kurino <kousaku.maron@gmail.com>
2019-10-11 03:59:43 +0900

    feat: add animation to search box

--- src/components/pages/SearchUserPage.tsx
+++ src/components/pages/SearchUserPage.tsx
@@ -1,16 +1,39 @@
-import React, { useState, useCallback } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useRef, useCallback } from 'react'
+import { View, ScrollView, TouchableOpacity, Animated, StyleSheet } from 'react-native'
 import { FullScreenModal } from '../templates'
-import { TextInput } from '../atoms'
+import { TextBox } from '../atoms'
 
 type Props = {
   isVisible: boolean
   onClose: () => void
   onChangeUserID?: (userID: string) => void
+  onSubmitUserID?: (userID: string) => void
+}
+
+const useSearchButton = () => {
+  const buttonWidth = useRef(new Animated.Value(0)).current
+  const buttonPaddingLeft = useRef(new Animated.Value(0)).current
+
+  const show = useCallback(() => {
+    Animated.parallel([
+      Animated.timing(buttonWidth, { toValue: 50, duration: 200 }),
+      Animated.timing(buttonPaddingLeft, { toValue: 8, duration: 200 })
+    ]).start()
+  }, [buttonPaddingLeft, buttonWidth])
+
+  const hidden = useCallback(() => {
+    Animated.parallel([
+      Animated.timing(buttonWidth, { toValue: 0, duration: 200 }),
+      Animated.timing(buttonPaddingLeft, { toValue: 0, duration: 200 })
+    ]).start()
+  }, [buttonPaddingLeft, buttonWidth])
+
+  return { buttonWidth, buttonPaddingLeft, show, hidden }
 }
 
 const SearchUserPage = (props: Props) => {
   const [value, setValue] = useState<string>('')
+  const searchButtonTools = useSearchButton()
 
   const onChangeText = useCallback(
     (text: string) => {
@@ -22,17 +45,46 @@ const SearchUserPage = (props: Props) => {
     [props]
   )
 
+  const onSubmitEditing = useCallback(() => {
+    if (props.onSubmitUserID) {
+      props.onSubmitUserID(value)
+    }
+  }, [props, value])
+
   return (
     <FullScreenModal title="Nomoca" isVisible={props.isVisible} onClose={props.onClose}>
       <View style={styles.container}>
         <View style={styles.headWrapper}>
-          <Text>友達のIDを検索してください。</Text>
-          <TextInput value={value} onChangeText={onChangeText} fullWidth={true} />
+          <View style={styles.searchBox}>
+            <TextBox
+              placeholder={'友達のIDを検索'}
+              value={value}
+              onChangeText={onChangeText}
+              onSubmitEditing={onSubmitEditing}
+              onEndEditing={searchButtonTools.hidden}
+              onFocus={searchButtonTools.show}
+              fullWidth={true}
+            />
+          </View>
+
+          <TouchableOpacity onPress={onSubmitEditing}>
+            <Animated.Text
+              style={[
+                styles.buttonText,
+                { width: searchButtonTools.buttonWidth, paddingLeft: searchButtonTools.buttonPaddingLeft }
+              ]}
+            >
+              検索
+            </Animated.Text>
+          </TouchableOpacity>
         </View>
+        <ScrollView></ScrollView>
       </View>
     </FullScreenModal>
   )
 }
+
+// const hairlineWidth = StyleSheet.hairlineWidth
 
 const styles = StyleSheet.create({
   container: {
@@ -40,8 +92,20 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   headWrapper: {
-    paddingTop: 48,
-    width: 300
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 12,
+    paddingVertical: 6
+    // borderBottomWidth: hairlineWidth,
+    // borderBottomColor: 'gray'
+  },
+  searchBox: {
+    flex: 1
+  },
+  buttonText: {
+    fontSize: 18
   }
 })
 
