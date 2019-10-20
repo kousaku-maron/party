2760da998a1289171a38842d669286b64eb82026
tabikaeru <moriokayui@eisti.eu>
2019-10-21 02:17:04 +0900

    modify:Modalposition_in_HomeDcreen

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack'
 import { HomeScreen, PartyEntryScreen, UserScreen, UserEditScreen } from '../containers'
-import { PostScreen } from '../screens'
+import { PostScreen, PartyDetailScreen } from '../screens'
 import { MaterialCommunityIcons, AntDesign, FontAwesome } from '@expo/vector-icons'
 
 type IconProps = {
@@ -34,7 +34,8 @@ const UserIcon = ({ tintColor, focused }: IconProps) => {
 const HomeNavigator = createStackNavigator(
   {
     Main: HomeScreen,
-    PartyEntry: PartyEntryScreen
+    PartyEntry: PartyEntryScreen,
+    PartyDetail: PartyDetailScreen
   },
   {
     initialRouteName: 'Main'
