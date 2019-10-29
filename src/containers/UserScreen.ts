bf9e42afa984f60e22e9558ee498c07e4938c8a8
Kosaku Kurino <kousaku.maron@gmail.com>
2019-10-29 20:36:31 +0900

    Revert "セッティング画面の作成"

--- src/containers/UserScreen.ts
+++ src/containers/UserScreen.ts
@@ -1,14 +1,30 @@
+import { Action, AnyAction } from 'typescript-fsa'
+import { Dispatch } from 'redux'
 import { connect } from 'react-redux'
 import { AppState } from '../reducers/configureStore'
 import { Auth } from '../reducers/auth'
+import { authActions, Fetch } from '../actions'
 import { UserScreen } from '../screens'
 
 export interface UserScreenState {
   auth: Auth
 }
 
+export interface UserScreenActions {
+  signOut: (fetch: Fetch) => Action<void>
+}
+
 const mapStateToProps = (appState: AppState) => ({
   auth: appState.auth
 })
 
-export default connect(mapStateToProps)(UserScreen)
+const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => ({
+  signOut: (fetch: Fetch) => {
+    dispatch(authActions.signOut(fetch))
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserScreen)
