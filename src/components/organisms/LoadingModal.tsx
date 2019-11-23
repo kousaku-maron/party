ffb1f94b03d851824d4fef8fea65915b758077af
kosaku kurino <kousaku.maron@gmail.com>
2019-11-23 11:55:58 +0900

    style: use theme color to loading modal

--- src/components/organisms/LoadingModal.tsx
+++ src/components/organisms/LoadingModal.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { View, ActivityIndicator, Platform, Dimensions, StyleSheet } from 'react-native'
 import Modal from 'react-native-modal'
+import { useStyles, useColors, MakeStyles } from '../../services/design'
 
 type Props = {
   isVisible: boolean
 }
 
 const LoadingModal: React.FC<Props> = props => {
+  const styles = useStyles(makeStyles)
+  const colors = useColors()
+
   return (
     <View>
       <Modal style={styles.container} isVisible={props.isVisible} animationIn="bounceIn" animationOut="fadeOut">
         <View style={styles.inner}>
-          <ActivityIndicator style={styles.indicator} size="large" color="gray" />
+          <ActivityIndicator style={styles.indicator} size="large" color={colors.system.gray} />
         </View>
       </Modal>
     </View>
@@ -20,24 +24,25 @@ const LoadingModal: React.FC<Props> = props => {
 
 const width = Dimensions.get('window').width
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  inner: {
-    display: 'flex',
-    backgroundColor: 'rgba(20, 20, 20, 0.8)',
-    borderRadius: Platform.OS === 'ios' ? 16 : 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: width * 0.6,
-    height: width * 0.6
-  },
-  indicator: {
-    transform: [{ scale: 1.5 }]
-  }
-})
+const makeStyles: MakeStyles = colors =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    inner: {
+      display: 'flex',
+      backgroundColor: colors.backgrounds.tertiary,
+      borderRadius: Platform.OS === 'ios' ? 16 : 3,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: width * 0.6,
+      height: width * 0.6
+    },
+    indicator: {
+      transform: [{ scale: 1.5 }]
+    }
+  })
 
 export default LoadingModal
