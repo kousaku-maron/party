f32071724a7490b88220f492254225325b3d33c1
kosaku kurino <kousaku.maron@gmail.com>
2019-11-27 02:57:59 +0900

    style: balloon bubble message style

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -29,13 +29,13 @@ const ChatScreen = ({ navigation }: Props) => {
 
   const renderBubble = useCallback(
     props => (
-      <View style={styles.bubbleWrapper}>
+      <View style={props.currentMessage.user._id === uid ? styles.bubbleRightWrapper : styles.bubbleLeftWrapper}>
         {props.currentMessage.user._id !== uid && <Text style={styles.username}>{props.currentMessage.user.name}</Text>}
         <CustomBubble
           {...props}
           containerStyle={{
-            left: { paddingTop: 8 },
-            right: { paddingTop: 8 }
+            left: { paddingTop: 3 },
+            right: { paddingTop: 3 }
           }}
           containerToPreviousStyle={{ left: { borderTopLeftRadius: 15 }, right: { borderTopRightRadius: 15 } }}
           containerToNextStyle={{ left: { borderBottomLeftRadius: 15 }, right: { borderBottomRightRadius: 15 } }}
@@ -51,6 +51,11 @@ const ChatScreen = ({ navigation }: Props) => {
             return null
           }}
         />
+        {props.currentMessage.user._id === uid ? (
+          <View style={styles.rightTriangle} />
+        ) : (
+          <View style={styles.leftTriangle} />
+        )}
         <View style={props.currentMessage.user._id !== uid ? styles.pinnedRight : styles.pinnedLeft}>
           <Text style={styles.createdAt}>{formattedDateForMessageCreatedAt(props.currentMessage.createdAt)}</Text>
         </View>
@@ -61,10 +66,13 @@ const ChatScreen = ({ navigation }: Props) => {
       colors.foregrounds.onTintPrimary,
       colors.foregrounds.primary,
       colors.tints.primary.light,
-      styles.bubbleWrapper,
+      styles.bubbleLeftWrapper,
+      styles.bubbleRightWrapper,
       styles.createdAt,
+      styles.leftTriangle,
       styles.pinnedLeft,
       styles.pinnedRight,
+      styles.rightTriangle,
       styles.username,
       uid
     ]
@@ -102,10 +110,16 @@ const makeStyles: MakeStyles = colors =>
     flex: {
       flex: 1
     },
-    bubbleWrapper: {
+    bubbleRightWrapper: {
       position: 'relative',
       maxWidth: '80%',
-      paddingBottom: 10.5
+      paddingBottom: 6,
+      paddingRight: 6
+    },
+    bubbleLeftWrapper: {
+      position: 'relative',
+      maxWidth: '80%',
+      paddingBottom: 6
     },
     pinnedRight: {
       position: 'absolute',
@@ -126,6 +140,32 @@ const makeStyles: MakeStyles = colors =>
       fontSize: 8,
       backgroundColor: 'transparent',
       color: colors.foregrounds.secondary
+    },
+    rightTriangle: {
+      position: 'absolute',
+      top: 12,
+      right: -10 + 6,
+      width: 0,
+      height: 0,
+      borderTopColor: colors.tints.primary.light,
+      borderTopWidth: 16,
+      borderBottomColor: 'transparent',
+      borderBottomWidth: 16,
+      borderRightColor: 'transparent',
+      borderRightWidth: 16
+    },
+    leftTriangle: {
+      position: 'absolute',
+      top: 24,
+      left: -10,
+      width: 0,
+      height: 0,
+      borderTopColor: colors.backgrounds.secondary,
+      borderTopWidth: 16,
+      borderBottomColor: 'transparent',
+      borderBottomWidth: 16,
+      borderLeftColor: 'transparent',
+      borderLeftWidth: 16
     }
   })
 
