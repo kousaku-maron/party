1dc7dc4ffeca8ba2dfcc2df47443a190c4fd1e88
kosaku kurino <kousaku.maron@gmail.com>
2019-11-28 20:46:21 +0900

    fix: date MM to mm

--- src/services/formatedDate.ts
+++ src/services/formatedDate.ts
@@ -1,13 +1,13 @@
 import moment from 'moment'
 
 export const formatedDateFull = (fetchDate: Date) => {
-  return String(moment(fetchDate).format('YYYY年MM月DD日HH:MM'))
+  return String(moment(fetchDate).format('YYYY年MM月DD日HH:mm'))
 }
 
 export const formatedDateMonthDateHour = (fetchDate: Date) => {
-  return String(moment(fetchDate).format('MM月DD日HH:MM'))
+  return String(moment(fetchDate).format('MM月DD日HH:mm'))
 }
 
 export const formattedDateForMessageCreatedAt = (date: Date) => {
-  return moment(date).format('HH:MM')
+  return moment(date).format('HH:mm')
 }
