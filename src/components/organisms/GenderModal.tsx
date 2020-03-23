79c637e305b03320824f7f068af3cd6141570da8
tabikaeru <moriokayuixxx@gmail.com>
2020-03-23 18:46:28 +0900

    refactor:move Modal organisms to moleculers

--- src/components/organisms/GenderModal.tsx
+++ src/components/organisms/GenderModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Modal from './Modal'
+import Modal from '../moleculers/Modal'
 import { Picker, StyleSheet, View } from 'react-native'
 import { useStyles, MakeStyles } from '../../services/design'
 
