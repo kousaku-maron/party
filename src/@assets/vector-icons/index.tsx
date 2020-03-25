ce955ed5664c6fa0aac8fe515fd2376b0c5f03f7
kosaku kurino <kousaku.maron@gmail.com>
2020-03-26 01:51:05 +0900

    feat: update icons

--- src/@assets/vector-icons/index.tsx
+++ src/@assets/vector-icons/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { HomeIcon } from './home'
 import { ChatIcon } from './chat'
+import { ChatPlusIcon } from './chat-plus'
+import { CheckIcon } from './check'
+import { EditIcon } from './edit'
+import { HomeIcon } from './home'
+import { PlusIcon } from './plus'
+import { SendIcon } from './send'
+import { SendFillIcon } from './send-fill'
+import { TimeIcon } from './time'
 import { UserIcon } from './user'
+import { UserPlusIcon } from './user-plus'
 
-type VectorIconsName = 'home' | 'chat' | 'user'
+type VectorIconsName = string
 
 export type VectorIconsProps = {
   name: VectorIconsName
@@ -13,16 +21,48 @@ export type VectorIconsProps = {
 
 export const Icons = ({ name, size = 24, color = 'black' }: VectorIconsProps) => {
   switch (name) {
-    case 'home': {
-      return <HomeIcon size={size} color={color} />
-    }
-
     case 'chat': {
       return <ChatIcon size={size} color={color} />
     }
 
+    case 'chat-plus': {
+      return <ChatPlusIcon size={size} color={color} />
+    }
+
+    case 'check': {
+      return <CheckIcon size={size} color={color} />
+    }
+
+    case 'edit': {
+      return <EditIcon size={size} color={color} />
+    }
+
+    case 'home': {
+      return <HomeIcon size={size} color={color} />
+    }
+
+    case 'plus': {
+      return <PlusIcon size={size} color={color} />
+    }
+
+    case 'send': {
+      return <SendIcon size={size} color={color} />
+    }
+
+    case 'send-fill': {
+      return <SendFillIcon size={size} color={color} />
+    }
+
+    case 'time': {
+      return <TimeIcon size={size} color={color} />
+    }
+
     case 'user': {
       return <UserIcon size={size} color={color} />
+    }
+
+    case 'user-plus': {
+      return <UserPlusIcon size={size} color={color} />
     }
 
     default:
