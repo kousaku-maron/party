e34d489fb7a93ee3d93cf65273dac2dcb0402f0f
tabikaeru <moriokayuixxx@gmail.com>
2020-03-27 16:56:39 +0900

    feature:add enabled to GroupAsset

--- src/entities/GroupAsset.ts
+++ src/entities/GroupAsset.ts
@@ -4,16 +4,19 @@ export type GroupAsset = {
   id: string
   groupID: string
   group: Group
+  enabled: boolean
 }
 
 export const buildGroupAsset = (id: string, data: firebase.firestore.DocumentData) => {
   const newGroupAsset: GroupAsset = {
     id,
     groupID: data.groupID,
-    group: buildGroup(data.group.id, data.group)
+    group: buildGroup(data.group.id, data.group),
+    enabled: data.enabled
   }
 
   return newGroupAsset
 }
 
 export type CreateGroupAsset = Omit<GroupAsset, 'id'>
+export type UpdateMyGroupAsset = { group: Group }
