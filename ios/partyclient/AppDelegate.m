00507e0a662d3eb1372ca2aed604fd5c22ec4d1e
kosaku kurino <kousaku.maron@gmail.com>
2020-04-23 23:07:42 +0900

    feat: add ios settings

--- ios/partyclient/AppDelegate.m
+++ ios/partyclient/AppDelegate.m
@@ -13,6 +13,8 @@
 #import <UMReactNativeAdapter/UMNativeModulesProxy.h>
 #import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
 
+@import Firebase;
+
 @implementation AppDelegate
 
 @synthesize window = _window;
@@ -22,6 +24,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure]; // Firebase初期化
+
   self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"partyclient" initialProperties:nil];
