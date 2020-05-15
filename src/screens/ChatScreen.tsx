3dc14e59282f9b8b5b84aa38bef693b9e3d0d2a8
kosaku kurino <kousaku.maron@gmail.com>
2020-05-15 23:46:34 +0900

    feat: add infinite scroll to chat

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react'
-import { View, ScrollView, StyleSheet, Dimensions, Text } from 'react-native'
+import React, { useCallback, useRef } from 'react'
+import { View, FlatList, StyleSheet, Dimensions } from 'react-native'
+import Animated, { Value, Extrapolate, interpolate } from 'react-native-reanimated'
 import { useRoute, RouteProp } from '@react-navigation/native'
 import { useStackNavigation } from '../services/route'
 import KeyboardSpacer from 'react-native-keyboard-spacer'
@@ -13,13 +14,19 @@ import { ShadowBase } from '../components/atoms'
 import { ChatBubble, ChatInput, Header } from '../components/organisms'
 import { User } from '../entities'
 
+const HEADER_HEIGHT = 50 + 24 + 6 // height + paddingTop + paddingBottom
+const TAB_HEIGHT = 70 // height
+const ON_END_REACHED_THRESHOLD = 30
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
+
 const ChatScreen = () => {
   const { uid } = useAppAuthState()
   const navigation = useStackNavigation()
   const inset = useSafeArea()
   const route = useRoute<RouteProp<RouteParams, 'Chat'>>()
   const roomID = route.params.roomID
-  const { fetching, messages } = useMessages(roomID)
+  const { fetching, messages, onNext } = useMessages(roomID)
   const { onSend } = useSendMessage(roomID)
   const styles = useStyles(makeStyles)
 
@@ -30,42 +37,84 @@ const ChatScreen = () => {
     [navigation]
   )
 
+  const scrollY = useRef(new Value<number>(0))
+
+  const hbOpacity = useRef(
+    interpolate(scrollY.current, {
+      inputRange: [0, 50],
+      outputRange: [0, 1],
+      extrapolate: Extrapolate.CLAMP
+    })
+  )
+
+  const onEndReachedCalled = useRef(false)
+
+  const onEndReached = useCallback(async () => {
+    if (fetching || onEndReachedCalled.current) return
+    onEndReachedCalled.current = true
+    await onNext()
+    onEndReachedCalled.current = false
+  }, [fetching, onNext])
+
   return (
     <NormalLayout fetching={fetching}>
       <View style={styles.container}>
-        <ScrollView
-          style={[styles.scrollView, { paddingTop: inset.top }]}
-          stickyHeaderIndices={[1]}
-          // メッセージは、スクロールできる長さが不確実なためインジゲーターを表示させることにしている。
-          // showsVerticalScrollIndicator={false}
-        >
-          <View style={styles.headerTopSpacer} />
-
-          <View style={styles.headerContainer}>
-            <Header />
+        <View style={[styles.headerContainer, { paddingTop: 24 + inset.top }]}>
+          <View style={styles.headerInner}>
+            <Header fullWidth={true} title="トークルーム" />
           </View>
+        </View>
 
-          {!fetching && messages.length === 0 && (
-            <View style={styles.emptyMessageContainer}>
-              <Text style={styles.emptyMessageText}>あいさつしてみよう！</Text>
-            </View>
-          )}
+        <Animated.View style={[styles.headerBackground, { paddingTop: 24 + inset.top, opacity: hbOpacity.current }]} />
 
-          {messages.map(message => {
-            const isMy = message.user.uid === uid
+        <AnimatedFlatList
+          style={[styles.scrollView, { paddingTop: HEADER_HEIGHT + inset.top + 24 }]}
+          showsVerticalScrollIndicator={false}
+          inverted={-1} // MEMO: 上下逆さまにしている。
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  y: scrollY.current
+                }
+              }
+            }
+          ])}
+          onEndReachedThreshold={ON_END_REACHED_THRESHOLD}
+          onEndReached={onEndReached}
+          data={messages}
+          renderItem={({ item }) => {
+            const isMy = item.user.uid === uid
 
             return (
-              <View key={message.id} style={isMy ? styles.myBubbleContainer : styles.bubbleContainer}>
+              <View key={item.id} style={isMy ? styles.myBubbleContainer : styles.bubbleContainer}>
                 <ShadowBase>
-                  <ChatBubble message={message} isMy={isMy} onPressAvatar={onPressAvatar} />
+                  <ChatBubble message={item} isMy={isMy} onPressAvatar={onPressAvatar} />
                 </ShadowBase>
               </View>
             )
-          })}
+          }}
+          // ListEmptyComponent={() => {
+          //   if (!fetching && messages.length === 0) {
+          //     return (
+          //       <View style={styles.emptyMessageContainer}>
+          //         <Text style={styles.emptyMessageText}>あいさつしてみよう！</Text>
+          //       </View>
+          //     )
+          //   }
 
-          {/* MEMO: tab height 70px */}
-          <View style={{ paddingBottom: inset.bottom + 70 + 200 }} />
-        </ScrollView>
+          //   return null
+          // }}
+          ListFooterComponent={() => <View style={{ paddingBottom: inset.top + HEADER_HEIGHT + 200 }} />}
+          ListHeaderComponent={() => (
+            <View
+              style={{
+                paddingBottom: messages.length > 3 ? inset.bottom + TAB_HEIGHT + 200 : inset.bottom + TAB_HEIGHT + 400
+              }}
+            />
+          )}
+        />
 
         <View style={[styles.tabContainer, { paddingBottom: inset.bottom }]}>
           <ShadowBase intensity={2}>
@@ -83,17 +132,34 @@ const fullWidth = Dimensions.get('window').width
 const makeStyles: MakeStyles = colors =>
   StyleSheet.create({
     container: {
+      width: '100%',
+      height: '100%',
       backgroundColor: colors.backgrounds.primary
     },
-    scrollView: {
-      display: 'flex',
-      width: '100%',
-      height: '100%'
-    },
     headerContainer: {
-      width: '100%',
-      paddingHorizontal: 24
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      zIndex: 1000,
+      elevation: 1000,
+      paddingBottom: 6
     },
+    headerBackground: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      zIndex: 999,
+      elevation: 999,
+      paddingBottom: 6,
+      height: 124,
+      backgroundColor: colors.backgrounds.tertiary
+    },
+    headerInner: {
+      paddingHorizontal: 36
+    },
+    scrollView: {},
     emptyMessageContainer: {
       width: '100%',
       height: 400,
@@ -113,17 +179,14 @@ const makeStyles: MakeStyles = colors =>
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'flex-end',
-      paddingRight: 14,
+      paddingRight: 12,
       paddingBottom: 20
     },
     tabContainer: {
       position: 'absolute',
       bottom: 0,
       width: fullWidth,
-      paddingHorizontal: 24
-    },
-    headerTopSpacer: {
-      paddingBottom: 36
+      paddingHorizontal: 12
     },
     emptyMessageText: {
       fontSize: 16,
